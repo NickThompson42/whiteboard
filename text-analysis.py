68125bbc1ded577c0c2ee44944f2f514fba2420a
Nick Thompson <49480739+NickThompson42@users.noreply.github.com>
2022-11-09 08:34:17 -0600

    successful pdf downloader
    finished the successful pdf downloader in text-analysis.py. This filename will change to pdf-downloader and text-analysis will become the base script for conducting text analysis.

--- text-analysis.py
+++ text-analysis.py
@@ -1,5 +1,45 @@
 # Import libraries
+import os
 import requests
-from bs4 import beautifulsoup4
+from urllib.parse import urljoin
+from bs4 import BeautifulSoup
+import datetime
 
-print("import successful")
+now = datetime.datetime.now()
+
+# URL where the pdfs are located
+url = "https://ahec.armywarcollege.edu/CENTCOM-IRAQ-papers/"
+
+# Folder location
+folder_location = r"/Users/nthompson/Downloads"
+if not os.path.exists(folder_location):os.mkdir("/Users/nthompson/Downloads")
+
+# Requests URL and get response object
+response = requests.get(url)
+
+# Parse text obtained
+soup = BeautifulSoup(response.text, 'html.parser')
+
+"""
+# Find all hyperlinks present on webpage
+links = soup.find_all('a')
+
+i = 0
+"""
+
+print("All complete before the `for` loop. Time now: ")
+print(now.strftime("%Y-%m-%d %H:%M:%S"))
+
+
+# From all links check for pdf link and
+# if present download file
+for link in soup.select("a[href$='.pdf']"):
+    # Name the pdf files using the last portion of each link which are unique
+    filename = os.path.join(folder_location, link['href'].split('/')[-1])
+    with open(filename, 'wb') as f:
+        f.write(requests.get(urljoin(url, link['href'])).content)
+
+   
+print("All PDF files downloaded")
+print("import successful at ")
+print(now.strftime("%Y-%m-%d %H:%M:%S"))
